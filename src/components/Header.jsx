b88637c90c61e34261745b2f6489d7ba132bc40c
nurulamin092 <mnurulamin092@gmial.com>
2022-07-23 21:56:39 +0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../img/logo.png';
+import Avatar from '../img/avatar.png';
+import { MdShoppingBasket } from 'react-icons/md'
 
 const Header = () => {
     return (
-        <div className='w-screen fixed z-50 bg-slate-300 p-6 px-16'>
+        <header className='w-screen fixed z-50 p-6 px-16'>
             {/*desktop and tablet */}
-            <div className='hidden md:flex w-full h-full bg-red-600 p-4'></div>
+            <div className='hidden md:flex w-full h-full items-center justify-between'>
+                <div className='flex items-center gap-2'>
+                    <img src={Logo} alt="logo" className='w-8 object-cover' />
+                    <p className='text-headingColor text-xl font-bold'>City</p>
+                </div>
+                <div className='flex items-center gap-8'>
+                    <ul className='flex items-center gap-8'>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
+
+                    </ul>
+                    <div className='relative flex items-center justify-center'>
+                        <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
+                        <div className=' absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                            <p className='text-sm text-white font-semibold'>2</p>
+                        </div>
+                    </div>
+                    <img className='w-10 min-w-[40px] h-10 min-h-[40px] drop- shadow-2xl' src={Avatar} alt="userProfile" />
+                </div>
+
+            </div>
             {/* mobile*/}
-            <div className='flex md:hidden w-full h-full bg-blue-500 p-4'></div>
-        </div>
+            <div className='flex md:hidden w-full h-full'></div>
+        </header>
     )
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -44,12 +44,12 @@ module.exports = {
         xl: "1280px",
         "2xl": "1536px",
       },
-      color: {
+      colors: {
         headingColor: "#2e2e2e",
         textColor: "#515151",
         cartNumBg: "#e80013",
         primary: "#f5f3f3"
-      }
+      },
     },
   },
   plugins: [],
