f2366e0d52fb0d9028cd974dd5f5322f5c04e07c
nurulamin092 <mnurulamin092@gmial.com>
2022-07-23 16:29:21 +0600

    tailwind setup done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            App
+            <h1 className='w-screen h-screen flex items-center justify-center text-blue-600 text-5xl'>Bismillah</h1>
 
         </div>
     );
