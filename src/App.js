cb05eade81f6ea72e753d121129220729b9ae229
nurulamin092 <mnurulamin092@gmial.com>
2022-07-23 16:51:25 +0600

    package install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Header } from './components';
 
 const App = () => {
     return (
         <div>
-            <h1 className='w-screen h-screen flex items-center justify-center text-blue-600 text-5xl'>Bismillah</h1>
-
+            <h1 className='w-screen h-auto flex flex-col'></h1>
+            <Header />
         </div>
     );
 };
