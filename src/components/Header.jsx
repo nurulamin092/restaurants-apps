36221875b09106fbc6669455cdf81cb60a471a9a
nurulamin092 <mnurulamin092@gmial.com>
2022-07-25 23:06:01 +0600

    new item add dropdown added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Logo from '../img/logo.png';
 import Avatar from '../img/avatar.png';
-import { MdShoppingBasket } from 'react-icons/md';
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -13,12 +13,15 @@ const Header = () => {
     const provider = new GoogleAuthProvider();
     const [{ user }, dispatch] = useStateValue();
     const login = async () => {
-        const { user: { refreshToken, providerData },
-        } = await signInWithPopup(firebaseAuth, provider);
-        dispatch({
-            type: actionType.SET_USER,
-            user: providerData[0],
-        });
+        if (!user) {
+            const { user: { refreshToken, providerData },
+            } = await signInWithPopup(firebaseAuth, provider);
+            dispatch({
+                type: actionType.SET_USER,
+                user: providerData[0],
+            });
+            localStorage.setItem('user', JSON.stringify(providerData[0]));
+        }
     };
     return (
         <header className='w-screen fixed z-50 p-6 px-16'>
@@ -46,11 +49,21 @@ const Header = () => {
                         <motion.img
                             whileTap={{ scale: 0.6 }}
                             src={user ? user.photoURL : Avatar}
-                            className='w-10 min-w-[40px] h-10 min-h-[40px]  drop- shadow-2xl cursor-pointer rounded-full'
-                            alt="userProfile" onClick={login} />
+                            className='w-10 min-w-[40px] h-10 min-h-[40px] 
+                             drop- shadow-2xl cursor-pointer rounded-full'
+                            alt="userProfile"
+                            onClick={login}
+                        />
+                        <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
+                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+                            transition-all duration-100 ease-in-out text-textColor text-base'>New Item <MdAdd />
+                            </p>
+                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100
+                             transition-all duration-100 ease-in-out text-textColor text-base'>Logout <MdLogout />
+                            </p>
+                        </div>
                     </div>
                 </div>
-
             </div>
             {/* mobile*/}
             <div className='flex md:hidden w-full h-full'></div>
