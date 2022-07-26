8ff9137d6bd7a65cd4977d5a94d16bc6a9a6725a
nurulamin092 <mnurulamin092@gmial.com>
2022-07-26 09:19:54 +0600

    sign out impiment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,6 +32,14 @@ const Header = () => {
             setIsMenu(!isMenu)
         }
     };
+    const logout = () => {
+        setIsMenu(false)
+        localStorage.clear();
+        dispatch({
+            type: actionType.SET_USER,
+            user: null
+        })
+    }
     return (
         <header className='w-screen fixed z-50 p-3 px-4 md:p-6 md:px-16'>
             {/*desktop and tablet */}
@@ -85,7 +93,7 @@ const Header = () => {
                                         )
                                     }
                                     <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100
-                             transition-all duration-100 ease-in-out text-textColor text-base'>Logout <MdLogout />
+                             transition-all duration-100 ease-in-out text-textColor text-base' onClick={logout}>Logout <MdLogout />
                                     </p>
                                 </motion.div>
                             )
@@ -135,8 +143,11 @@ const Header = () => {
                                     <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer  hover:bg-slate-100 px-4 py-2'>About Us</li>
 
                                 </ul>
-                                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100
-                             transition-all duration-100 ease-in-out text-textColor text-base'>Logout <MdLogout />
+                                <p className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer
+                                 hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base '
+                                    onClick={logout}
+                                >
+                                    Logout <MdLogout />
                                 </p>
                             </motion.div>
                         )
