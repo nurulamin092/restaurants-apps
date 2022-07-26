a278723b670a5c02876d0900ec1a3603b5410e01
nurulamin092 <mnurulamin092@gmial.com>
2022-07-26 10:32:07 +0600

    maint conteiner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -103,6 +103,12 @@ const Header = () => {
             </div>
             {/* mobile*/}
             <div className='flex md:hidden w-full h-full items-center justify-between'>
+                <div className='relative flex items-center justify-center'>
+                    <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
+                    <div className=' absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                        <p className='text-sm text-white font-semibold'>2</p>
+                    </div>
+                </div>
                 <Link to={'/'} className='flex items-center gap-2'>
                     <img src={Logo} alt="logo" className='w-8 object-cover' />
                     <p className='text-headingColor text-xl font-bold'>City</p>
