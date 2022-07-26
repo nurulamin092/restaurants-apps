9cd4892d5f785ba9b70727de21fa058e0ec15dee
nurulamin092 <mnurulamin092@gmial.com>
2022-07-26 19:49:25 +0600

    home container daynamic data added

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import Delivery from '../img/delivery.png'
 import HeroBg from '../img/heroBg.png'
 import I1 from '../img/i1.png'
+import { heroData } from '../utils/data'
 
-const heroData = [
-    { id: 1, name: 'Ice cream', description: 'Chocolate & Vanilla', price: '5.25', imageSrc: '' }
-]
 
 const HomeContainer = () => {
     return (
@@ -36,14 +34,32 @@ const HomeContainer = () => {
             <div className="py-2 flex-1 items-center relative">
 
                 <img src={HeroBg} className="ml-auto h-370 w-full lg:w-auto lg:h-650" alt="heroBg" />
-                <div className='w-full h-full absolute top-0 left-0  flex items-center justify-center px-32 py-4'>
-                    <div className='w-190  bg-cardOverlay backdrop-blue-md rounded-3xl p-4 flex flex-col items-center justify-center'>
-                        <img src={I1} className='w-40 -mt-20 ' alt="i1" />
-                        <p className='text-xl font-semibold text-textColor mt-4'>Ice Cream</p>
-                        <p className='text-sm text-lightTextGray font-semibold my-3 '>Chocolate & Vanilla</p>
-                        <p className="text-sm font-semibold text-headingColor"><span className='text-xs text-red-600'>$</span>5.25</p>
+                <div className='w-full h-full absolute top-0 left-0  flex items-center justify-center 
+               lg:px-32 py-4 gap-4 flex-wrap'>
+                    {
+                        heroData && heroData.map(n => (
+                            <div key={n.id} className='lg:w-190  p-4
+                              bg-cardOverlay backdrop-blue-md rounded-3xl  
+                              flex flex-col items-center justify-center drop-shadow-lg'>
+                                <img src={n.imageSrc} className='w-20 lg:w-40 -mt-10 lg:-mt-20' alt="i1" />
 
-                    </div>
+                                <p className='text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4'>
+                                    {n.name}
+                                </p>
+                                <p
+                                    className='text-[12px] lg:text-sm text-lightTextGray font-semibold my-1 lg:my-3'>
+                                    {n.description}
+                                </p>
+                                <p className="text-sm font-semibold text-headingColor">
+                                    <span className='text-xs text-red-600'>
+                                        $
+                                    </span>
+                                    {n.price}
+                                </p>
+
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
