517b6a7801b8fe0105ecd9dfdeb2bc206125940c
nurulamin092 <mnurulamin092@gmial.com>
2022-07-27 22:16:49 +0600

    addet calories and price input field

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
-import { MdFastfood, MdCloudUpload } from 'react-icons/md'
+import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from 'react-icons/md'
 import { categories } from '../utils/data';
 import Loader from './Loader';
 const CreateContainer = () => {
@@ -14,8 +14,18 @@ const CreateContainer = () => {
     const [msg, setMsg] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
+
+    const uploadImage = () => {
+
+    };
+
+    const deleteImage = () => {
+
+    }
+
+
     return (
-        <div className='w-full min-h-screen flex items-center justify-center'>
+        <div className='w-full min-h-screen flex items-center justify-center mb-20'>
             <div className='w-[90%] md:w-[75%] border  border-gray-300 rounded-lg p-4 
             flex flex-col items-center justify-center gap-4'>
                 {
@@ -38,8 +48,8 @@ const CreateContainer = () => {
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
                         placeholder="Give me a title..."
-                        className='w-full h-full text-lg bg-transparent font-semibold 
-                        outline-none border-none placeholder:text-gray-500 text-textColor'
+                        className='w-full h-full text-lg bg-transparent
+                        outline-none border-none placeholder:text-gray-400 text-textColor'
                         required
                     />
                 </div>
@@ -63,25 +73,89 @@ const CreateContainer = () => {
                 <div className='group flex justify-center items-center flex-col 
                 border-2 border-dotted rounded-lg border-gray-300 w-full h-225 md:h-420 cursor-pointer'>
                     {
-                        isLoading ? <Loader /> : <>
-                            {
-                                !imageAsset ? <>
-                                    <label className='w-full h-full flex flex-col
+                        isLoading ?
+                            (
+                                <Loader />
+                            )
+                            : <>
+                                {
+                                    !imageAsset ? (
+                                        <>
+                                            <label className='w-full h-full flex flex-col
                                      items-center justify-center cursor-pointer'>
-                                        <div className='w-full h-full flex flex-col
+                                                <div className='w-full h-full flex flex-col
                                      items-center justify-center cursor-pointer gap-2'>
-                                            <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700' />
-                                            <p className='text-gray-500 text-3xl hover:text-gray-700'>Click here to upload</p>
-                                        </div>
-                                    </label>
-                                </> : <></>
-                            }
-                        </>
+                                                    <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700' />
+                                                    <p className='text-gray-500 text-3xl hover:text-gray-700'>
+                                                        Click here to upload
+                                                    </p>
+                                                </div>
+                                                <input
+                                                    type="file"
+                                                    name='uploadimage'
+                                                    accept='image/*'
+                                                    onChange={uploadImage}
+                                                    className='w-0 h-0'
+                                                />
+                                            </label>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <div className='relative h-full'>
+                                                <img
+                                                    src={imageAsset} alt="uploaded img"
+                                                    className='w-full h-full object-cover'
+                                                />
+                                                <button
+                                                    type='button'
+                                                    className='absolute bottom-3 
+                                                    right-3 p-3 rounded-all bg-red-500 
+                                                    text-xl cursor-pointer outline-none hover:shadow-md
+                                                    duration-500 transition-all ease-in-out'
+                                                    onClick={deleteImage}
+                                                >
+                                                    <MdDelete className='text-white' />
+                                                </button>
+                                            </div>
+                                        </>
+                                    )
+                                }
+                            </>
                     }
+                </div>
+                <div className='w-full flex flex-col md:flow-row items-center gap-3'>
+                    <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+                        <MdFoodBank className='text-gray-400 text-2xl' />
+                        <input
+                            type="text"
+                            required
+                            value={calories}
+                            onChange={(e) => setCalories(e.target.value)}
+                            placeholder='Calories'
+                            className='w-full h-full text-lg  bg-transparent 
+                            outline-node border-none placeholder:border-gray-400
+                             text-textColor'
+
+                        />
+                    </div>
+
+                    <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+                        <MdAttachMoney className='text-gray-400 text-2xl' />
+                        <input
+                            type="text"
+                            required
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                            placeholder='Price'
+                            className='w-full h-full text-lg  bg-transparent 
+                            outline-node border-none placeholder:border-gray-400 text-textColor'
+
+                        />
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default CreateContainer
